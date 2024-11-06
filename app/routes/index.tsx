2eb67738fe94f128855a1f2e64fe89348f88cee4
Tom Ballinger <tom@convex.dev>
2024-11-06 12:03:04 -0800

    working

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,22 @@
+import { convexQuery } from '@convex-dev/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { api } from 'convex/_generated/api'
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
+  const { data } = useSuspenseQuery(convexQuery(api.tasks.get, {}))
+  const { data: authData } = useSuspenseQuery(
+    convexQuery(api.tasks.getAuth, {}),
+  )
   return (
     <div className="p-2">
       <h1>Hello Clerk!</h1>
+      <code>{JSON.stringify(authData)}</code>
+      <code>{JSON.stringify(data)}</code>
     </div>
   )
 }
