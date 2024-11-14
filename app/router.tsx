edbfc7b5211eb9c94aab9fdd604b581d40cd8d44
Tom Ballinger <tom@convex.dev>
2024-11-13 16:14:17 -0800

    just add Convex

--- app/router.tsx
+++ app/router.tsx
@@ -2,14 +2,49 @@ import { createRouter as createTanStackRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
 import { NotFound } from './components/NotFound'
+import { QueryClient } from '@tanstack/react-query'
+import { routerWithQueryClient } from '@tanstack/react-router-with-query'
+import { ConvexQueryClient } from '@convex-dev/react-query'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
 
 export function createRouter() {
-  const router = createTanStackRouter({
-    routeTree,
-    defaultPreload: 'intent',
-    defaultErrorComponent: DefaultCatchBoundary,
-    defaultNotFoundComponent: () => <NotFound />,
+  const CONVEX_URL = (import.meta as any).env.VITE_CONVEX_URL!
+  if (!CONVEX_URL) {
+    console.error('missing envar VITE_CONVEX_URL')
+  }
+  const convexQueryClient = new ConvexQueryClient(CONVEX_URL)
+
+  const queryClient: QueryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryKeyHashFn: convexQueryClient.hashFn(),
+        queryFn: convexQueryClient.queryFn(),
+      },
+    },
   })
+  convexQueryClient.connect(queryClient)
+
+  const router = routerWithQueryClient(
+    createTanStackRouter({
+      routeTree,
+      defaultPreload: 'intent',
+      defaultErrorComponent: DefaultCatchBoundary,
+      defaultNotFoundComponent: () => <NotFound />,
+      context: { queryClient, convexClient: convexQueryClient },
+      Wrap: ({ children }) => (
+        <ClerkProvider publishableKey="pk_test_Y2xlcmsubWF4aW11bS5sb25naG9ybi0xMC5sY2wuZGV2JA">
+          <ConvexProviderWithClerk
+            client={convexQueryClient.convexClient}
+            useAuth={useAuth}
+          >
+            {children}
+          </ConvexProviderWithClerk>
+        </ClerkProvider>
+      ),
+    }),
+    queryClient,
+  )
 
   return router
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,18 @@
+import { convexQuery } from '@convex-dev/react-query'
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
+import { api } from 'convex/_generated/api'
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
+  const { data } = useSuspenseQuery(convexQuery(api.tasks.get, {}))
+  console.log(data);
   return (
     <div className="p-2">
-      <h1>Hello Clerk!</h1>
+      <h1>Hello Clerk + Convex!</h1>
     </div>
   )
 }
